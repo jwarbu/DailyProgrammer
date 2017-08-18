9606d71ac002d2cb6ad1e0b5cfeb583b305a3570
jwarbu <jwartick@bu.edu>
2017-08-18 16:10:11 -0400

    8/18, 10:09am - updated challenge text with notes/planning - working on main code

--- Java/Easy/Easy_321/E321.java
+++ Java/Easy/Easy_321/E321.java
@@ -10,110 +10,21 @@ public class E321
 {
     public static void main(String[] args) throws IOException
     {
-        try
-        {
-            File file = new File("Java/Easy/Easy_321/input.txt");
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String readLine = "";
-            List<String> times = new ArrayList<String>();
-            
-            System.out.println("Reading file ...");
-            while((readLine = br.readLine()) != null)
-            {
-                times.add(readLine);
-            }
-
-            String[] spoken_times = new String[times.size()];
-            int loop = times.size();
-            for(int i = 0; i<loop; i++)
-            {
-                List<String> full_time = Arrays.asList(times.get(i).split(":")); //should contain hour in (0) and minutes in (1)
-                String hour = full_time.get(0);
-                String minutes = full_time.get(1);
-                
-                switch(hour)
-                {
-                    case "00":
-                    case "12":
-                    {
-                        full_time.add(0, "It's twelve ");
-                        break;
-                    }
-                    case "01":
-                    case "13":
-                    {
-                        full_time.add(0, "It's one ");
-                        break;
-                    }
-                    case "02":
-                    case "14":
-                    {
-                        full_time.add(0, "It's two ");
-                        break;
-                    }
-                    case "03":
-                    case "15":
-                    {
-                        full_time.add(0, "It's three ");
-                        break;
-                    }
-                    case "04":
-                    case "16":
-                    {
-                        full_time.add(0, "It's four ");
-                        break;
-                    }
-                    case "05":
-                    case "17":
-                    {
-                        full_time.add(0, "It's five ");
-                        break;
-                    }
-                    case "06":
-                    case "18":
-                    {
-                        full_time.add(0, "It's six ");
-                        break;
-                    }
-                    case "07":
-                    case "19":
-                    {
-                        full_time.add(0, "It's seven ");
-                        break;
-                    }
-                    case "08":
-                    case "20":
-                    {
-                        full_time.add(0, "It's eight ");
-                        break;
-                    }
-                    case "09":
-                    case "21":
-                    {
-                        full_time.add(0, "It's nine ");
-                        break;
-                    }
-                    case "10":
-                    case "22":
-                    {
-                        full_time.add(0, "It's ten ");
-                        break;
-                    }
-                    case "11":
-                    case "23":
-                    {
-                        full_time.add(0, "It's eleven ");
-                        break;
-                    }
-                }
-                
-                
-            }
-        } 
+        List<String> times = new ArrayList<>();
+        times.add("00:00");
+        times.add("01:30");
+        times.add("12:05");
+        times.add("14:01");
+        times.add("20:29");
+        times.add("21:00");
         
-        catch(IOException e)
+        int loop = times.size();
+        for(int i = 0; i<loop; i++)
         {
-            e.printStackTrace();
+            List<String> full_time = Arrays.asList(times.get(i).split(":")); //should contain hour in (0) and minutes in (1)
+            String hour = full_time.get(0);
+            String minutes = full_time.get(1);
+            
         }
     }
 }
